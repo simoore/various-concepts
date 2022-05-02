f44f1cd4a9a86450cd907ea50fcdbcd43a860b21
Steven Moore <steven.ian.moore@gmail.com>
2022-05-02 21:54:43 +1000

    Started minigrep project.

--- rust-intro/minigrep/src/main.rs
+++ rust-intro/minigrep/src/main.rs
@@ -1,3 +1,44 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    // collect() transforms the iterator returned by args() into a collection.
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    run(config);
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        // The name of the binary as it was called on the command line is in arg[0].
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config { query, filename })
+    }
 }
