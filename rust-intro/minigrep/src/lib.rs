83be5c366e587ab7302ab1e7dc3792d9650aa799
Steven Moore <steven.ian.moore@gmail.com>
2022-05-04 14:30:57 +1000

    Implemented iterator logic in minigrep project.

--- rust-intro/minigrep/src/lib.rs
+++ rust-intro/minigrep/src/lib.rs
@@ -9,14 +9,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();    // The name of the binary as it was called on the command line is in arg[0].
 
-        // The name of the binary as it was called on the command line is in arg[0].
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string."),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name."),
+        };
     
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -25,24 +29,18 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
--- rust-intro/minigrep/src/main.rs
+++ rust-intro/minigrep/src/main.rs
@@ -3,10 +3,7 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    // collect() transforms the iterator returned by args() into a collection.
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
